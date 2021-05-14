a69c676adfcfd178d75dbb575aa57ed7d5f2f1f3
Minseokang <minseo_0606@naver.com>
2021-05-15 00:57:46 +0900

    (Add) : Add 2runo's-Curse-detection-data.txt and prepare to read

---
+++ dataset/reading.py
@@ -0,0 +1,8 @@
+from data_transform.transform import Transform_to_txt_csv
+import pandas as pd
+
+tt = Transform_to_txt_csv() # tt라는 인스턴스에 Transform_to_txt_csv를 할당
+tt.load('korean/2runo\'s-Curse-detection-data.txt') # 데이터 로딩하기
+data = tt.read() # 데이터 읽기
+
+# 2runo\'s-Curse-detection-data.txt 에 있는 글자를 읽은후 data의 저장까지 완료
