d611cf1c97acffb91f975d22ebbb4fa1d768a915
Minseokang <minseo_0606@naver.com>
2021-05-15 11:15:04 +0900

    (feat) : change to txt -> csv

--- dataset/reading.py
+++ dataset/reading.py
@@ -1,8 +1,24 @@
 from data_transform.transform import Transform_to_txt_csv
 import pandas as pd
-
+import csv
 tt = Transform_to_txt_csv() # tt라는 인스턴스에 Transform_to_txt_csv를 할당
 tt.load('korean/2runo\'s-Curse-detection-data.txt') # 데이터 로딩하기
 data = tt.read() # 데이터 읽기
+ls = list(data.split('\n'))
+out = []
+for i in ls:
+    out.append(i.split('|'))
 
-# 2runo\'s-Curse-detection-data.txt 에 있는 글자를 읽은후 data의 저장까지 완료 :)
+sentence_ls = []
+targetls = []
+
+for i in out:
+    sentence_ls.append(i[0])
+    targetls.append(i[1])
+
+df = pd.DataFrame(columns=['sentence', 'target'],
+                  data = {
+                      'sentence' : sentence_ls,
+                      'target' : targetls
+                  })
+df.to_csv('korean/2runo\'s-swear.csv')
