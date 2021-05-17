75345837ee31aab5067ef1e7ca22fb8fbd1305ae
Minseokang <minseo_0606@naver.com>
2021-05-17 09:51:21 +0900

    (Add) : make one_hot_encoding

--- CoCo_AI/dataset/data_preprocessing/string.py
+++ CoCo_AI/dataset/data_preprocessing/string.py
@@ -4,17 +4,42 @@ def return_to_split_list(input_ls : list) -> list:
     :param input_ls:
     :return:
     """
-    two_dimensional_paramls = []
+    two_dimensional_paramls = split_the_list_original(input_ls)
     outls = []
-    for input_string in input_ls:
-        appendls = list(str(input_string).split())
-        two_dimensional_paramls.append(appendls)
     for index_string_list in two_dimensional_paramls:
         for index_string in index_string_list:
             outls.append(str(index_string))
     return outls
 
+def split_the_list_original(input_ls : list) -> list:
+    two_dimensional_paramls = []
+    for input_string in input_ls:
+        appendls = list(str(input_string).split())
+        two_dimensional_paramls.append(appendls)
+    return two_dimensional_paramls
+
+def one_hot_encoding(input_ls : list) -> list:
+    """
+    원핫 인코딩을 사용하여 단어를 인코딩
+    :param input_ls:
+    :return:
+    """
+    dict_loop_index = 0
+    dict_loop = {}
+    set_input_ls = set(return_to_split_list(input_ls))
+    for st in set_input_ls:
+        dict_loop[f"{st}"] = dict_loop_index
+        dict_loop_index += 1
+    string_ls = split_the_list_original(input_ls)
+    outls = []
+    output = []
+    for i in string_ls:
+        for j in i:
+            outls.append(dict_loop[j])
+        output.append(outls)
+        outls = []
+    return output
 
 
 if __name__ == '__main__':
-    print(return_to_split_list((['좌배 까는건 ㅇㅂ', '좌배 아져씨 바보'])))
+    print(one_hot_encoding((['좌배 까는건 ㅇㅂ', '좌배 아져씨 바보'])))
