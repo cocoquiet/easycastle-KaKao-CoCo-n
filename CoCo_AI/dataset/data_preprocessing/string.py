1aff9413e89bbbecc1c39827b6ea143f60b25904
Minseokang <minseo_0606@naver.com>
2021-05-28 23:47:24 +0900

    (feat) : Fix the random init to token_encoding issues

--- CoCo_AI/dataset/data_preprocessing/string.py
+++ CoCo_AI/dataset/data_preprocessing/string.py
@@ -1,3 +1,21 @@
+import pandas as pd
+
+def split_the_list_original(input_ls : list) -> list:
+    two_dimensional_paramls = []
+    for input_string in input_ls:
+        appendls = list(str(input_string).split())
+        two_dimensional_paramls.append(appendls)
+    return two_dimensional_paramls
+
+def change_df_to_dict(input_df):
+    number = input_df['number']
+    sentence = input_df['sentence']
+    ls = [[n,s] for n,s in zip(sentence, number)]
+    out_dict = {}
+    for i in ls:
+        out_dict[f"{i[0]}"] = i[1]
+    return out_dict
+
 def return_to_split_list(input_ls : list) -> list:
     """
     문자열로 입력된 리스트를 공백기준으로 자르는 함수입니다.
@@ -11,31 +29,15 @@ def return_to_split_list(input_ls : list) -> list:
             outls.append(str(index_string))
     return outls
 
-def split_the_list_original(input_ls : list) -> list:
-    """
-    문자열로 입력된 리스트를 공백기준으로 잘라, 2차원 배열으로 묶기
-    :param input_ls:
-    :return:
-    """
-    two_dimensional_paramls = []
-    for input_string in input_ls:
-        appendls = list(str(input_string).split())
-        two_dimensional_paramls.append(appendls)
-    return two_dimensional_paramls
-
 def token_encoding(input_ls : list) -> list:
     """
     토큰 인코딩을 사용하여 단어를 인코딩
     :param input_ls:
     :return:
     """
-    dict_loop_index = 0
-    dict_loop = {}
-    set_input_ls = set(return_to_split_list(input_ls))
-    for st in set_input_ls:
-        dict_loop[f"{st}"] = dict_loop_index
-        dict_loop_index += 1
+    dict_loop = change_df_to_dict(pd.read_csv("/content/sentence.csv"))
     string_ls = split_the_list_original(input_ls)
+    out_ls = []
     outls = []
     output = []
     for i in string_ls:
@@ -44,6 +46,7 @@ def token_encoding(input_ls : list) -> list:
         output.append(outls)
         outls = []
     return output
+
 
 if __name__ == '__main__':
     while True:
--- CoCo_AI/dataset/reading.py
+++ CoCo_AI/dataset/reading.py
@@ -1,6 +1,5 @@
 from data_preprocessing.files_transform import Transform_to_txt_csv
 import pandas as pd
-import csv
 tt = Transform_to_txt_csv() # tt라는 인스턴스에 Transform_to_txt_csv를 할당
 tt.load('korean/2runo\'s-Curse-detection-data.txt') # 데이터 로딩하기
 data = tt.read() # 데이터 읽기
